5b5766cc3c6b8656696952743915414a3c2b6013
aea53 <aea53@cam.ac.uk>
2025-11-11 13:47:47 +0000

    Add files via upload
    URM09 Ultrasonic sensor
    Range 2cm to 500cm

---
+++ URM09 Ultrasonic Sensor.py
@@ -0,0 +1,13 @@
+from machine import Pin, ADC
+from libs.tcs3472_micropython.tcs3472 import tcs3472
+from utime import sleep
+
+MAX_RANGE = 520
+ADC_SOLUTION = 3300.0
+sen = ADC(Pin(26))
+
+while True:
+    adc_value = sen.read_u16()
+    dist_t = adc_value * MAX_RANGE/ ADC_SOLUTION 
+    print(dist_t)
+    sleep(0.2)
