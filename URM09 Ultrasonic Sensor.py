421d64ecedcdb6e41b9a31e98b1ce54c6a5ee498
aea53 <aea53@cam.ac.uk>
2025-11-11 16:32:16 +0000

    Update URM09 Ultrasonic Sensor.py
    removed unused import

--- URM09 Ultrasonic Sensor.py
+++ URM09 Ultrasonic Sensor.py
@@ -1,5 +1,4 @@
 from machine import Pin, ADC
-from libs.tcs3472_micropython.tcs3472 import tcs3472
 from utime import sleep
 
 MAX_RANGE = 520
@@ -10,4 +9,5 @@ while True:
     adc_value = sen.read_u16()
     dist_t = adc_value * MAX_RANGE/ ADC_SOLUTION 
     print(dist_t)
+
     sleep(0.2)
