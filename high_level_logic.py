9f7446b94b12de0f1df390af8785b88d35d35ce4
AranA-eng <aa2622@cam.ac.uk>
2025-11-26 13:26:47 +0000

    Comments where pins need to be checked
    Added comments to clarify I2C bus configurations.

--- high_level_logic.py
+++ high_level_logic.py
@@ -25,7 +25,7 @@ right_motor = mo.Motor(RIGHT_DIR, RIGHT_PWM)
 actuator = act.Actuator(dirPin, PWMPin)
 
 # i2c buses																											
-i2c_colour = I2C(id = 0, sda = Pin(8), scl = Pin(9), freq = 400000)
+i2c_colour = I2C(id = 0, sda = Pin(8), scl = Pin(9), freq = 400000)           #check these
 i2c_VL53L0X = I2C(id = 0, sda = Pin(8), scl = Pin(9))                         #on the left?
 i2c_TMF8701 = I2C(id = 1, sda = Pin(2), scl = Pin(3))                         #on the right?
 
