09e25b45792395b953726b85445c451271cd2641
Yoonjae Hwang <yh583@cam.ac.uk>
2025-11-26 13:55:01 +0000

    point a to point b

--- tests/navigation_test.py
+++ tests/navigation_test.py
@@ -24,9 +24,11 @@ IR_PINS = [12, 13, 11, 10]
 line_sensor_weights = [-5.0, -1.0, 1.0, 5.0]
 IR_sensors = sen.IRSensorArray( IR_PINS, line_sensor_weights)
 
-Kp = 2300.0
-Ki = -20.0
-Kd = -7.5
+# Controller parameters
+# Gain
+Kp = -2300.0
+Ki = 20.0
+Kd = 7.5
 
 dt_ms = 10 # control loop period in ms
 base_speed = 45000 # base speed
@@ -36,8 +38,8 @@ drive = mo.DiffDrive(left_motor, right_motor)
 jh = JunctionHandler(drive, 0, max_pwm)
 count = 0
 
-# map of the nodes
 nodedirections = {
+    -1: (10, 10, 1, -1, 0), #note that for the minor nodes, minordirs means the direction to turn at their major links to go f or b
     0: (10, 10, 1, -1, -1),
     1: (10, 10, 1, -1, 39),
     2: (-1, 1, 1, 10, 40),
@@ -77,11 +79,17 @@ nodedirections = {
     36: (10, 10, -1, 1, None),
     37: (10, 10, -1, 1, None),
     38: (10, 2, 10, 10, None),
-    39: (10, 10, 10, 10, 1), #minor from 1
-    40: (10, 10, 10, 10, 2), #minor from 2
-    41: (10, 10, 10, 10, 20), #minor from 20
-    42: (10, 10, 10, 10, 20), #minor from 21
+    39: (10, 10, 1, -1, 1), #minor from 1
+    40: (10, 10, 10, -1, 2), #minor from 2
+    41: (10, 10, 1, 10, 20), #minor from 20
+    42: (10, 10, 1, -1, 21), #minor from 21
 }
+
+graph = rt.RouteGraph(nodedirections) 
+router = rt.Router(graph)
+junction_list = router.route(-1, 22)
+turns = junction_list.turn_sequence
+nodes = junction_list.path_nodes
 
 def turn_follower(junction_list, vals, count):
     """ when sensor reads a junction, the junction identification is given by the corresponding counter
@@ -90,34 +98,29 @@ def turn_follower(junction_list, vals, count):
         junction = junction_list[count]
         next_count = count + 1 # next index in the junction_list
         return junction, next_count
-    
-graph = rt.RouteGraph(nodedirections)
-router = rt.Router(graph)
-junction_list = router.route(0,22)
-turns = junction_list.turn_sequence
-nodes = junction_list.path_nodes
+
+
 
 try: 
     while True: 
         t_start = time.ticks_ms()
-
         vals = IR_sensors.read()
-        err = sen.IRSensorArray.compute_error(vals)
+        err = IR_sensors.compute_error(vals)
         corr = pid.update(err)
-
-        # CURRENT JUNCTION
-        current_node = nodes[count]
-        
         # once new node is detected, returns the behaviour that needs to happen, as well as incrementing count
+        
         result = turn_follower(turns, vals, count)
         if result: 
             junction, count = result
         else: 
             junction = 0 # go straight
-
+        
+        
+        jh.apply_motor_speeds(base_speed, corr, junction)
+        
         elapsed = time.ticks_diff(time.ticks_ms(), t_start)
         if elapsed < dt_ms:
             time.sleep_ms(dt_ms - elapsed)
 
 finally:
-    mo.DiffDrive.stop()
+    drive.stop()
