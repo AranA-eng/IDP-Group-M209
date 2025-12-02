d4c5f9e4af87474a92a0967ecb94d5ab0d9f8c6c
AranA-eng <aa2622@cam.ac.uk>
2025-12-02 15:12:06 +0000

    Update junction_handler.py

--- control/junction_handler.py
+++ control/junction_handler.py
@@ -51,7 +51,7 @@ class JunctionHandler: # handling left and right motor
     
         elif dir == 2:
             sleep(0.2)
-                self.motors.set(-55000, 55000)
+            self.motors.set(-55000, 55000)
             sleep(2)
 
     
